03ee9143f629fab05789927800503c702d58c62b
hammadx02 <hammadaali02@gmail.com>
2024-08-06 21:45:09 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:smart_home_animation/core/app/app.dart';
 
 void main() {
-  // runApp(const SmartHomeApp());
+  runApp(const SmartHomeApp());
 }
